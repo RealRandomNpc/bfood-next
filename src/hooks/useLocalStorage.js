e3dd99d6772c2e6b831049e2bced9255435bdd51
bs <benysit1234@gmail.com>
2024-12-01 14:07:37 +0200

    change: useLocalStorage hook

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -7,22 +7,31 @@ const useLocalStorage = (key, defaultValue) => {
   const [value, setValue] = useState(defaultValue);
 
   useEffect(() => {
-    if (isMounted.current) {
-      window.localStorage.setItem(key, JSON.stringify(value));
-    } else {
-      isMounted.current = true;
+    try {
+      if (isMounted.current) {
+        window.localStorage.setItem(key, JSON.stringify(value));
+      } else {
+        isMounted.current = true;
+      }
+    } catch (error) {
+      console.log("STORAGE SET ERROR", error)
     }
   }, [value, key]);
 
   useLayoutEffect(() => {
-    const storageValue = window.localStorage.getItem(key) != null ? JSON.parse(window.localStorage.getItem(key)) : defaultValue;
-
-    if (storageValue) {
-      if (Array.isArray(storageValue) || typeof storageValue === 'object') {
-        storageValue.length > 0 && setValue(storageValue)
-      } else {
-        setValue(storageValue)
+    try {
+      const storageValue = window.localStorage.getItem(key) != null ? JSON.parse(window.localStorage.getItem(key)) : defaultValue;
+  
+      if (storageValue) {
+        if (Array.isArray(storageValue) || typeof storageValue === 'object') {
+          storageValue.length > 0 && setValue(storageValue)
+        } else {
+          setValue(storageValue)
+        }
       }
+      
+    } catch (error) {
+      console.log("STORAGE GET", error)
     }
 
     return () => {
